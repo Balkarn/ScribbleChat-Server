1ef55c41048213f5c71582c288a3a7e016105d25
Balkarn <57596334+Balkarn@users.noreply.github.com>
2020-08-31 19:20:04 +0100

    Improved documentation

--- app.js
+++ app.js
@@ -16,52 +16,64 @@ const io = socketIo(server);
 
 var names = [] //List of all names online
 
-io.on("connection", (socket) => { //on connection
+//When there is a connection from a new client
+io.on("connection", (socket) => {
 
     socket.name = null;
-
     console.log("New client connected");
 
+    //Request from the client to try a username
     socket.on('tryName', (nameAttempt) => {
+
         if (names.indexOf(nameAttempt) > -1) { //If name already exists
             socket.emit('nameAgain',nameAttempt);
         } else {
             io.emit
-            names.push(nameAttempt);
+            names.push(nameAttempt); //Store the name
             socket.name = nameAttempt;
-            socket.emit('nameValid',"");
+            socket.emit('nameValid',""); //Send a message back that it is valid
             console.log(names);
-            socket.broadcast.emit('userJoined',nameAttempt);
+            socket.broadcast.emit('userJoined',nameAttempt); //Tell everyone else that there is a new user
         }
+
     });
 
+    //Request from the client to know who is online
     socket.on('requestCurrentlyOnline', (data) => {
+
         toReturn = "Currently online:";
-        for (i = 0; i < names.length; i++) {
+        for (i = 0; i < names.length; i++) { //Build a string of everyone online
             if (names[i] != data) {
                 toReturn = toReturn + " " + names[i] + ",";
             } else {
-                toReturn = toReturn + " " + names[i] + "(you)"
+                toReturn = toReturn + " and you!";
             }
         }
-        socket.emit('currentlyOnline', toReturn);
+        socket.emit('currentlyOnline', toReturn); //Send a message back of the built string
+
     });
 
+    //Request from the cleint to send a message
     socket.on('sendMessage', (msg) => {
-        io.emit('recieveMessage', msg); //emit the chat message event to everyone
+        io.emit('recieveMessage', msg); //Emit the chat message event to everyone connected
     });
 
+    //Disconnection event
     socket.on("disconnect", () => {
-        socket.broadcast.emit('userLeft', socket.name);
+
+        socket.broadcast.emit('userLeft', socket.name); //Tell everyone else that they have left
         console.log("Client disconnected");
-        if (socket.name!=null) {
+
+        if (socket.name!=null) { //Remove the users name from the list of names online (if the name has been selected)
             var index = names.indexOf(socket.name);
             if (index > -1) {
                 names.splice(index, 1);
             }   
             console.log(names);
         }
+
     });
+
 });
 
 server.listen(port, () => console.log(`Listening on port ${port}`));
